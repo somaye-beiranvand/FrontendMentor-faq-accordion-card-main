f88e831ceaf89aac59e77d5ef3338a23f61379f3
unknown <beiranvand.somaye@gmail.com>
2022-08-23 23:27:53 +0430

    make mobile image dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,22 @@ $(document).ready(function () {
       return 90;
     } else return sm_box_container_padding;
   }
-
+  function img_mobile_height_cal() {
+    let sm_faq_img_width = $(".sm-faq-img-mobile").width();
+    let sm_faq_img_height = sm_faq_img_width / 1.09;
+    let sm_faq_img_mt = -sm_faq_img_height / 2;
+    $(".sm-faq-img-mobile").height(sm_faq_img_height);
+    return sm_faq_img_mt;
+  }
+  $(".sm-faq-img-mobile").css("margin-top", img_mobile_height_cal);
+  img_mobile_height_cal();
   $(".container").css("padding-top", padding_cal());
   $(".container").css("padding-bottom", padding_cal());
   $(window).resize(function () {
     $(".container").css("padding-top", padding_cal());
     $(".container").css("padding-bottom", padding_cal());
+    img_mobile_height_cal();
+    $(".sm-faq-img-mobile").css("margin-top", img_mobile_height_cal);
   });
   $(".sm-faq-head").on("click", function () {
     $(this).siblings(".sm-faq-body").stop();
