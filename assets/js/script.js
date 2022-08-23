ef47fc8d3b1d0162d317e005bfa7dbb20deff83a
unknown <beiranvand.somaye@gmail.com>
2022-08-23 21:50:02 +0430

    set padding for container using jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  function padding_cal() {
+    let window_height = $(window).outerHeight();
+    let sm_box_height = $(".sm-box").outerHeight();
+
+    let sm_box_container_padding = Math.abs(
+      (window_height - sm_box_height) / 2
+    );
+    if (sm_box_container_padding < 85) {
+      return 85;
+    } else return sm_box_container_padding;
+  }
+
+  $(".container").css("padding-top", padding_cal());
+  $(".container").css("padding-bottom", padding_cal());
+  $(window).resize(function () {
+    $(".container").css("padding-top", padding_cal());
+    $(".container").css("padding-bottom", padding_cal());
+  });
   $(".sm-faq-head").on("click", function () {
     $(this).siblings(".sm-faq-body").stop();
     $(this).toggleClass("sm-fw-bold");
